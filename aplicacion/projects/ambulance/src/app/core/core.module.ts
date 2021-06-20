89f6a41bee0a1e9b05599fa5d6aa91b121f83841
sergio <sergiohidalgocaceres@gmail.com>
2021-06-20 12:51:53 -0500

    agregado layout login

--- aplicacion/projects/ambulance/src/app/core/core.module.ts
+++ aplicacion/projects/ambulance/src/app/core/core.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginModule } from 'projects/login/src/lib/login.module';
 import { PageLoginComponent } from './views/pages/page-login/page-login.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [PageLoginComponent],
   exports: [PageLoginComponent],
-  imports: [LoginModule],
+  imports: [LoginModule, FlexLayoutModule, MatIconModule],
 })
 export class CoreModule {}
