464a0e04c0a5d8f74bae40c4e39a6d08342d5899
sergio <sergiohidalgocaceres@gmail.com>
2021-06-27 09:43:57 -0500

    layout - 1

--- aplicacion/projects/ambulance/src/app/app.module.ts
+++ aplicacion/projects/ambulance/src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { OwnComponent } from './own.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 @NgModule({
   declarations: [AppComponent, OwnComponent],
-  imports: [BrowserModule, CoreModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- aplicacion/projects/ambulance/src/app/core/core.module.ts
+++ aplicacion/projects/ambulance/src/app/core/core.module.ts
@@ -3,10 +3,21 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { LoginModule } from 'projects/login/src/lib/login.module';
 import { PageLoginComponent } from './views/pages/page-login/page-login.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { HeaderComponent } from './views/components/header/header.component';
 
 @NgModule({
-  declarations: [PageLoginComponent],
-  exports: [PageLoginComponent],
-  imports: [LoginModule, FlexLayoutModule, MatIconModule],
+  declarations: [PageLoginComponent, HeaderComponent],
+  exports: [PageLoginComponent, HeaderComponent],
+  imports: [
+    LoginModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
 })
 export class CoreModule {}
